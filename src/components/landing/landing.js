abe00aca9cec33c7ef662ba74ffe4f24a327da64
Scott Young <scott.t.young1@gmail.com>
2018-06-13 21:20:28 -0700

    Media Queries Work

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -88,18 +88,19 @@ class Landing extends Component {
                     <div className="productBG">
                         <div className="wovenParent">
                             <div>
-                                <div>
+                                <div className="wovenCenterTitle">
                                     <h2 className="wovenTitle">PREMIUM COTTON,  <br/>
                                                                LUXURIOUS SHEETS 
                                     </h2>
                                 </div>
-                                <div>
-                                    <h5 className="wovenSubtitle">100% AMERICAN-GROWN <br/> 
-                                                                  SUPIMA COTTON
-                                    </h5>
+                                <div className="wovenCenterSubtitle">
+                                    <div className="wovenSubtitle"> 100% AMERICAN-GROWN </div>  
+                                    <div className="wovenSubtitle2"> SUPIMA COTTON</div>                      
                                 </div>
-                                <div className="btn">
-                                    <div className="btn-black">VIEW PRODUCT</div>
+                                <div className="wovenCenterBtn">
+                                    <div className="btn">
+                                        <div className="btn-black">VIEW PRODUCT</div>
+                                    </div>
                                 </div>
                             </div>
                             <div>
@@ -113,20 +114,58 @@ class Landing extends Component {
                         <div className="linenParent">
                             <div>
                                 <img className="linenImgs" src={Linen1}/>
+                                <div className="linensTitle"> LUXURIOUS FEEL </div>
+                                <div className="linenParagraphs">
+                                This superior extra-long staple cotton is
+                                100 percent American grown. The fibers
+                                are an impressive 35 percent longer than
+                                upland cotton and produce smoother,
+                                softer yarns. The result is luxurious 
+                                sateen sheets with an elegant drape.
+                            </div>
                             </div>
                             <div>
                                 <img className="warranty" src={year}/>
                                 <img className="linenImgs" src={Cotton}/>
+                                <div className="linensTitle"> SUPERIOR STRENGTH </div>
+                                <div className="linenParagraphs">
+                                Twice as strong as regular cotton,
+                                Supima cotton resists pilling, breaking,
+                                and tearing. Backed by our industry-
+                                leading 5-year warranty, this resilient
+                                fiber lasts longer than regular cotton and 
+                                keeps its brilliance through years of 
+                                washing.
+                            </div>
                             </div>
                             <div>
-                                <img className="linenImgs" src={Linen2}/>   
+                                <img className="linenImgs" src={Linen2}/>
+                                <div className="linensTitle"> BEAUTIFUL COLOR </div>
+                                <div className="linenParagraphs">
+                                Supima cotton is finely woven and
+                                mercerized to create fabric that is softer
+                                and more vibrantly colored. Enjoy our 
+                                elegant color palatte of White, Smoke,
+                                Flax, and Charcoal for truly luxurious
+                                sheeting that customers covet.
+                            </div>   
                             </div>
                             <div>
                                 <img className="linenImgs" src={Seal}/>
+                                <div className="linensTitle"> PROTECTED FIBER </div>
+                                <div className="linenParagraphs">
+                                Supima<sup><i id="trademark" className="fa fa-registered"></i></sup> is the trademark used to
+                                promote American-grown, extra-long
+                                staple cotton. To bear the Supima
+                                trademark, products must be sourced
+                                through a licensed supply chain. This 
+                                process protects buyers from false
+                                advertising. 
+                            </div>
                             </div>
                         </div>
 
-                        <div className="titleParent">
+                        {/* <div className="titleParent">
                             <div className="linensTitle">
                                 LUXURIOUS FEEL
                             </div>
@@ -139,9 +178,9 @@ class Landing extends Component {
                             <div className="linensTitle">
                                 PROTECTED FIBER
                             </div>
-                        </div>
+                        </div> */}
 
-                        <div className="paragraphParent">
+                        {/* <div className="paragraphParent">
                             <div className="linenParagraphs">
                                 This superior extra-long staple cotton is
                                 100 percent American grown. The fibers
@@ -176,7 +215,7 @@ class Landing extends Component {
                                 process protects buyers from false
                                 advertising. 
                             </div>
-                        </div>
+                        </div> */}
 
                         <div>
                             <a className="learnMore" href="#">Learn More About Supima &#8250;</a>
